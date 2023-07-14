d897f937a74763beaffedb725f372cf97b39cb2c
Rishabh Rawat <rishabh2001rawat@gmail.com>
2023-07-14 23:36:09 +0530

    cors

--- app.js
+++ app.js
@@ -18,10 +18,15 @@ config({
 //using middleware
 app.use(express.json());
 app.use(cookieParser());
+// app.use(cors({
+//   origin:[process.env.FRONTEND_URL],
+//   methods:["GET","POST","PUT","DELETE"],
+//   credentials:true,
+// }));
 app.use(cors({
-  origin:[process.env.FRONTEND_URL],
-  methods:["GET","POST","PUT","DELETE"],
-  credentials:true,
+  origin: '*',
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 }));
 
 //keep router at last
